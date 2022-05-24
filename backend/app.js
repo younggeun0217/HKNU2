13b15758d640aa146e7c5272088461be1558ac16
younggeun0217 <dudrms452@gmail.com>
2022-05-25 00:24:44 +0900

    OCR 추가 "tesseract.js" install

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var naverRouter = require("./routes/naver");
+
 const app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/naverNews", naverRouter);
+
 const cors = require("cors");
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
