a25dc4a8226806ab8714df8d154226a632d09cc6
publicminsu <publicminsu@naver.com>
2022-06-17 20:50:44 +0900

    안사용하는 부분 삭제

--- backend/python/wordToSimilar.py
+++ backend/python/wordToSimilar.py
@@ -1,6 +1,5 @@
 #flask,flask_cors, flask-restx,sentence_transformers 설치(오류시 관리자 권한 또는 --user)
 from sentence_transformers import SentenceTransformer, util
-import numpy as np
 from flask import Flask,request
 from flask_restx import Api,Resource
 embedder = SentenceTransformer("jhgan/ko-sroberta-multitask")
